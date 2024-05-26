96955df1d26ff662c1070be8a5b631e7f79f3634
Zachary Weiss <20050953+zacharyweiss@users.noreply.github.com>
2024-05-26 11:43:32 -0400

    Fix INT overlap w/ OK dpad btn

--- glue.h
+++ glue.h
@@ -3,7 +3,7 @@
 #define P_MOSI &gpio_ext_pa7
 #define P_CLK &gpio_ext_pb3
 #define P_CS &gpio_ext_pa4
-#define P_INT &gpio_ext_pc3
+#define P_INT &gpio_ext_pb2
 #define SPI_TIMEOUT 1000
 
 void board_init();
